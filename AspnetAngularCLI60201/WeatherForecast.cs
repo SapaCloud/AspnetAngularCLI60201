a96b25cf4823f1240541f7aed7824feb9e771743
nhatasu <97354608+nhatasu@users.noreply.github.com>
2022-03-31 16:19:10 -0700

    Original code

---
+++ AspnetAngularCLI60201/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace AspnetAngularCLI60201;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
